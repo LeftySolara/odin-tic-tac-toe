38f8d9b2e6577f65e9c8470a5e42a99ac54383f7
Jalen Adams <jalen@jalenkadams.me>
2020-06-06 11:12:13 -0400

    Add: skeletons for game, gameBoard, player, and displayController

--- game.js
+++ game.js
@@ -1,1 +1,24 @@
-alert("Hello World!");
+const Player = (name, tile) => {
+    const getName = () => name;
+    const getTile = () => tile;
+
+    return {getName, getTile};
+};
+
+let game = (function() {
+    /* TODO: implement me! */
+})();
+
+let gameBoard = (function() {
+    "use strict"
+
+    let board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ];
+})();
+
+let displayController = (function() {
+    /* TODO; implement me! */
+})();
