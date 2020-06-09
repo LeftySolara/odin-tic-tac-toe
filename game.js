39f875dc92b3accd01a3b14b3a57708c78cf0a2a
Jalen Adams <jalen@jalenkadams.me>
2020-06-09 19:10:05 -0400

    Add: functions for letting players mark board

--- game.js
+++ game.js
@@ -1,24 +1,53 @@
-const Player = (name, tile) => {
+const player = (name, tile) => {
     const getName = () => name;
     const getTile = () => tile;
 
     return {getName, getTile};
 };
 
-let game = (function() {
-    /* TODO: implement me! */
-})();
-
-let gameBoard = (function() {
+const gameBoard = (function() {
     "use strict"
 
-    let board = [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
-    ];
+    let _board = [];
+    let _currentTile = "";
+
+    function setCurrentTile(symbol) {
+        _currentTile = symbol;
+    }
+
+    function initialize() {
+        let squares = document.querySelectorAll(".boardSquare");
+        squares.forEach(square => {
+            square.addEventListener("click", function() {
+                square.innerHTML = _currentTile;
+            })
+            _board.push(square)
+        });
+    }
+
+    return {setCurrentTile, initialize}
+})();
+
+const game = (function() {
+    let _player1;
+    let _player2;
+    let _currentPlayer;
+    let _board = gameBoard;
+
+    function initialize() {
+        _player1 = player("Bob", "X");
+        _player2 = player("Alice", "O");
+        _currentPlayer = _player1;
+
+        _board.initialize();
+        _board.setCurrentTile(_currentPlayer.getTile());
+    }
+
+    return {initialize};
 })();
 
 let displayController = (function() {
     /* TODO; implement me! */
 })();
+
+game.initialize();
