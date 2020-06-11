a921153eca23a6b77eac309ba014d72509b5976f
Jalen Adams <jalen@jalenkadams.me>
2020-06-11 12:01:32 -0400

    Refactor: make game board into own module

--- game.js
+++ game.js
@@ -13,12 +13,8 @@ const player = (name, tile) => {
     return {marks, getName, getTile};
 };
 
-const game = (function() {
+const gameBoard = (function() {
     "use strict"
-
-    let _player1;
-    let _player2;
-    let _currentPlayer;
 
     let _board = [
         [null, null, null],
@@ -26,16 +22,13 @@ const game = (function() {
         [null, null, null]
     ];
 
-    function switchPlayer() {
-        if (_currentPlayer === _player1) {
-            _currentPlayer = _player2;
-        }
-        else {
-            _currentPlayer = _player1;
-        }
+    let _currentSymbol;
+
+    function setSymbol(symbol) {
+        _currentSymbol = symbol;
     }
 
-    function _checkForWinner(row, column, marks) {
+    function checkForWin(row, column, marks) {
         let boardSize = _board[0].length;
         marks.row[row]++;
         marks.column[column]++;
@@ -58,13 +51,49 @@ const game = (function() {
     function markBoard(row, column) {
         let square = _board[row][column];
         if (square.innerHTML === "") {
-            square.innerHTML = _currentPlayer.getTile();
-
-            if (_checkForWinner(row, column, _currentPlayer.marks)) {
-                alert(`${_currentPlayer.getName()} wins!`);
-            }
-            switchPlayer();
+            square.innerHTML = _currentSymbol;
         }
+    }
+
+    function initialize(turnFunc) {
+        let squares = Array.from(document.querySelectorAll(".boardSquare"));
+        for (let row = 0; row < _board.length; ++row) {
+            for (let col = 0; col < _board[0].length; ++col) {
+                let square = squares.shift();
+                square.addEventListener("click", function() {
+                    turnFunc(row, col);
+                });
+                _board[row][col] = square;
+            }
+        }
+    }
+
+    return {setSymbol, checkForWin, markBoard, initialize};
+})();
+
+const game = (function() {
+    "use strict"
+
+    let _player1;
+    let _player2;
+    let _currentPlayer;
+
+    function _switchPlayer() {
+        if (_currentPlayer === _player1) {
+            _currentPlayer = _player2;
+        }
+        else {
+            _currentPlayer = _player1;
+        }
+        gameBoard.setSymbol(_currentPlayer.getTile());
+    }
+
+    function performTurn(row, column) {
+        gameBoard.markBoard(row, column);
+        if (gameBoard.checkForWin(row, column, _currentPlayer.marks)) {
+            alert(`${_currentPlayer.getName()} wins!`);
+        }
+        _switchPlayer();
     }
 
     function initialize() {
@@ -72,16 +101,8 @@ const game = (function() {
         _player2 = player("Alice", "O");
         _currentPlayer = _player1;
 
-        let squares = Array.from(document.querySelectorAll(".boardSquare"));
-        for (let row = 0; row < _board.length; ++row) {
-            for (let col = 0; col < _board[0].length; ++col) {
-                let square = squares.shift();
-                square.addEventListener("click", function() {
-                    markBoard(row, col);
-                });
-                _board[row][col] = square;
-            }
-        }
+        gameBoard.initialize(performTurn);
+        gameBoard.setSymbol(_currentPlayer.getTile());
     }
 
     return {initialize};
